e7e45dda0804bba821a81b61e151d3bc74b61f3c
Ceyhun <bcatagec@gmail.com>
2024-02-15 14:11:51 +0300

    vite base config updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-movie-app-client',
+  base: '/react-movie-app-client/',
   plugins: [react()],
 })
